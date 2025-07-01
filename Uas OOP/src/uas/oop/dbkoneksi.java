e5464a5dd24e2e6caad36d13cc87566245778e84
linvi1706 <linvianti380@gmail.com>
2025-07-01 22:01:11 +0800

    Keterangan commit

---
+++ Uas OOP/src/uas/oop/dbkoneksi.java
@@ -0,0 +1,27 @@
+package UasOOP;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+/**
+ *
+ * linvianti
+ * TGL 01 Juli 2025
+ */
+public class dbkoneksi {
+    static String JDBC_DRIVER="com.mysql.cj.jdbc.Driver";
+    static String DB_USER="root";
+    static String DB_PASSWORD="";
+    static String DB_URL="jdbc:mysql://localhost:3306/mahasiswa";
+    
+    public static Connection koneksi(){
+        try{
+            Class.forName(JDBC_DRIVER);
+            return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            
+        }catch(ClassNotFoundException | SQLException err){
+            System.out.println("Gagal Koneksi Ke Database");
+        }
+        return null;
+    }
+}
